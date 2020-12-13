e5aa574b0587104e123f03f977bd7c212ed5535d
Hamzah Fayad <hamzahfayad99@gmail.com>
2020-12-13 14:00:57 +0100

    add more cards

---
+++ index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $('.card').hover(function() {
+        /*$(this).find(".card__content").toggleClass('active');
+        $(this).find(".card__image").toggleClass('active');
+        $(this).find(".card__footer").toggleClass('active');*/
+        //$(this).find(".card__content").toggleClass('transition');
+        //$(this).find(".card__footer").toggleClass('transition');
+
+        return false;
+    })
+  });
